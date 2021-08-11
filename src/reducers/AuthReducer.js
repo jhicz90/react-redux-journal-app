054dbb1f569756f7a888b5e8030e336af0fe4b15
jhicz90 <jhicz90@gmail.com>
2021-08-10 21:12:55 -0500

    first commit

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,22 @@
+// {
+//     uid: 'asdaa54sd1245da61s6',
+//     name:'Jose hans'
+// }
+
+import { types } from "../types/types";
+
+export const AuthReducer = (state = {}, action) => {
+    switch (action.type) {
+        case types.login:
+            return {
+                uid: action.payload.uid,
+                name: action.payload.displayName
+            }
+
+        case types.logout:
+            return {}
+
+        default:
+            return state
+    }
+}
